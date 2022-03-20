5aaf5cc1da540a9417ac16084fbec2f91fb94c99
monjurulkarim <raju.monjurulkarim@gmail.com>
2022-03-20 09:20:06 -0700

    initial commit

--- feat_extract/det_csv.py
+++ feat_extract/det_csv.py
@@ -1,43 +1,9 @@
-'''
-import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
-
-from src.model import FeatureExtractor
-from PIL import Image
-
-device = ("cuda" if torch.cuda.is_available() else "cpu")
-feat_extractor = 'resnet50'
-h_dim = 256
-transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5 ,0.5)),
-        ]
-    )
-image_path = 'new_file.jpg'
-
-image = Image.open(image_path)
-image = transform(image)
-image = torch.unsqueeze(image,0)
-
-print('=======================')
-print(image.shape)
-print('=======================')
-
-feat_extract = FeatureExtractor(device, h_dim+h_dim, feat_extractor)
-#
-#
-features = feat_extract(image)
-
-print('features shape : ', features)
-'''
 import pandas as pd
 import numpy as np
 
+anno_file = 'm1.csv' # annotation file that contains the video id, toa, and the risky objects traking number
+track_text = 'track_text/' # deepsort generated text files directory (video tracking information stored in this text file)
 
-anno_file = 'm1.csv' # annotation file that contains the video id and the risky objects traking number
 
 vid_ids = []
 # scaling_w = 4.82 # 1080/224 Unnecessary at this stage
@@ -54,7 +20,7 @@ with open(anno_file, 'r') as f:
         risk_objects = [int(num) for num in risk_object]
         toa = str(a[1])
         vid_ids.append(vid_id)
-        vid_text ='track_text/'+ vid_id+'.txt' #video tracking information stored in this text file
+        vid_text = track_text+ vid_id+'.txt' # deepsort generated text files directory (video tracking information stored in this text file)
         dummy_list = []
         with open(vid_text, 'r') as vid_text_file:
             for line in vid_text_file.readlines():
@@ -75,6 +41,7 @@ with open(anno_file, 'r') as f:
         for i in range(len(risk_objects)):
             df.loc[df['track_id'] == risk_objects[i],'label']= int(1)
         df.to_csv('csv_files/'+vid_id+'-'+toa+ '.csv', index=False) # a csv file will be stored corresponding to each video_id
+
 
 
 
--- feat_extract/feat_extract.py
+++ feat_extract/feat_extract.py
@@ -11,38 +11,29 @@ from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from src.model import FeatureExtractor
 from PIL import Image
-
+import argparse
 device = ("cuda" if torch.cuda.is_available() else "cpu")
 
-# transform = transforms.Compose(
-#         [   transforms.Resize(224),
-#             transforms.ToTensor(),
-#             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5 ,0.5)),
-#         ]
-#     )
+
 transform = transforms.Compose(
         [   transforms.Resize(224),
             transforms.ToTensor(),
-
         ]
     )
 to_pil_image = transforms.ToPILImage()
 
-# def featureExt(image):
-#     feat_extractor = 'resnet50'
-#     h_dim = 256
-#     # image = Image.open(img_path)
-#     image = transform(image)
-#     image = torch.unsqueeze(image,0).float().to(device=device)
-#     extractor = FeatureExtractor(device, h_dim+h_dim, feat_extractor).to(device=device)
-#     extractor.eval()
-#     with torch.no_grad():
-#         feat = extractor(image)
-#     return feat # 1x 512
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv_dir", default="csv_files") #csv files of each videos, contain tracking, bbox, etc information
+    parser.add_argument("--data_dir", default="data/") # contains the folderwise video frames
+    parser.add_argument("--feature_dir", default="feature/") # extracted resnet50 features will be stored here in *.npz format
+    args = parser.parse_args()
+    return args
 
 def featureExt(image):
     # image = Image.open(img_path)
-    image = transform(image) # 3 x 224 x 336
+    image = transform(image) # 3 x 224 x 336 (for the frame, for object it varies)
     # print('shape image : ', image.shape)
     image = torch.unsqueeze(image,0).float().to(device=device)
     extractor = FeatureExtractor().to(device=device)
@@ -57,24 +48,28 @@ def featureExt(image):
 def bbox_to_imroi(video_frame, bbox):
     image = Image.open(video_frame)
     image = transform(image)
-    imroi = image[:,bbox[1]:bbox[3],bbox[0]:bbox[2]]
+    imroi = image[:,bbox[1]:bbox[3],bbox[0]:bbox[2]] #(x1:x2, y1:y2)
     return imroi
 
 
 
 def main():
-    csv_files = natsorted(glob.glob(os.path.join('csv_files', '*.csv')))
+    args = get_args()
+    csv_dir = args.csv_dir
+    data_dir = args.data_dir
+    feature_dir = args.feature_dir
+    csv_files = natsorted(glob.glob(os.path.join(csv_dir, '*.csv')))
     # print(csv_files)
     scaling = 3.21
 
     for i in csv_files:
-        detections =  np.zeros((100,30,6),dtype=np.float32)
-        feature = np.zeros((100,31,2048),dtype=np.float32)
-        # feature = torch.zeros(100,35,512)
+        detections =  np.zeros((100,30,6),dtype=np.float32) # 100 frames, 30 maximum objects, 6: 1-> track_id, (2,3,4,5)-> (y1,x1;y2,x2), 6-> object serial number in each frame
+        feature = np.zeros((100,31,2048),dtype=np.float32) # 100 frames, 1 global frame-level feature + 30 object level feature , resnet50 feat dimension 2048
+
         vid_id_toa = i.split('.')[0].split('/')[-1]
         vid_id = vid_id_toa.split('-')[0]
-        toa = int(vid_id_toa.split('-')[-1])
-        video = 'data/'+vid_id+'/'
+        toa = int(vid_id_toa.split('-')[-1]) # time of accident
+        video = data_dir+vid_id+'/'
         video_frames = natsorted(glob.glob(os.path.join(video, '*.jpg')))
         print('========',vid_id,'===================')
         for j in range(len(video_frames)):
@@ -106,10 +101,8 @@ def main():
 
                 feature[f_num,row['object_num']+1,:] = object_feat
 
-        save_file = 'feature/'+ vid_id+'.npz'
+        save_file = feature_dir+ vid_id+'.npz'
         np.savez_compressed(save_file, feature = feature, detection = detections, vid_id= vid_id, toa=toa)
-
-
 
 if __name__ == '__main__':
     main()
