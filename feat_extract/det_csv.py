1f77d9f782217ada4b412038ad06bf0fe1917a77
monjurulkarim <raju.monjurulkarim@gmail.com>
2022-03-30 17:14:26 -0700

    roi resized and error log

--- feat_extract/det_csv.py
+++ feat_extract/det_csv.py
@@ -1,55 +1,59 @@
 import pandas as pd
 import numpy as np
 
-anno_file = 'm1.csv' # annotation file that contains the video id, toa, and the risky objects traking number
-track_text = 'track_text/' # deepsort generated text files directory (video tracking information stored in this text file)
+# annotation file that contains the video id, toa, and the risky objects traking number
+anno_file = 'm_set1_frames.csv'
+# deepsort generated text files directory (video tracking information stored in this text file)
+track_text = 'track_text/m_set1_frames/'
 
 
 vid_ids = []
-# scaling_w = 4.82 # 1080/224 Unnecessary at this stage
-# scaling_h = 3.21 #720/224
+
 with open(anno_file, 'r') as f:
     for line in f.readlines():
         a = line.split(",")
         print(a)
-        vid_id = a[0] # first element is video id
-        toa = str(a[1]) # 2nd element is toa
-        risk_object = a[2:-1] # risk objects are from 3rd
-        last_risk = a[-1].split('\n')[0] #excluding the new line \n
+        vid_id = a[0]  # first element is video id
+        toa = str(a[1])  # 2nd element is toa
+        risk_object = a[2:-1]  # risk objects are from 3rd
+        last_risk = a[-1].split('\n')[0]  # excluding the new line \n
         risk_object.append(last_risk)
         print(risk_object)
-        risk_objects = [int(num) for num in risk_object] # converting to int
+        risk_objects = [int(num) for num in risk_object]  # converting to int
 
-        vid_ids.append(vid_id) # Unnecessary
-        vid_text = track_text+ vid_id+'.txt' # deepsort generated text files directory (video tracking information stored in this text file)
+        vid_ids.append(vid_id)  # Unnecessary
+        # deepsort generated text files directory (video tracking information stored in this text file)
+        vid_text = track_text + vid_id+'.txt'
         dummy_list = []
         with open(vid_text, 'r') as vid_text_file:
             for line in vid_text_file.readlines():
                 a = line.split(" ")
-                b = a[:6] # we only need first 6 elements from the text file
-                c = [int(val) for val in b] # converting int
-                c[4] = c[2]+ c[4] # converting width (w) to coordinate y2
-                c[5] = c[3]+ c[5] # converting height (h) to coordinate x2
-                c[0]= c[0]-1 # frame number starts at 0
+                b = a[:6]  # we only need first 6 elements from the text file
+                c = [int(val) for val in b]  # converting int
+                c[4] = c[2] + c[4]  # converting width (w) to coordinate y2
+                c[5] = c[3] + c[5]  # converting height (h) to coordinate x2
+                c[0] = c[0]-1  # frame number starts at 0
                 dummy_list.append(c)
             vid_text_file.close()
-        df = pd.DataFrame(np.array(dummy_list), columns=['frame','track_id','y1','x1','y2','x2'])
+        df = pd.DataFrame(np.array(dummy_list), columns=[
+                          'frame', 'track_id', 'y1', 'x1', 'y2', 'x2'])
         unique, counts = np.unique(df['frame'].values, return_counts=True)
         df['object_num'] = None
         for i in range(len(unique)):
-            df.loc[df['frame'] == unique[i],'object_num'] =np.array(range(counts[i])) # counting the number of objects in each frame
-        df['label']= 0
+            df.loc[df['frame'] == unique[i], 'object_num'] = np.array(
+                range(counts[i]))  # counting the number of objects in each frame
+        df['label'] = 0
         for i in range(len(risk_objects)):
-            df.loc[df['track_id'] == risk_objects[i],'label']= int(1) # label 1= risky object. Assigning labels corresponding to risk object. 
-        df.to_csv('csv_files/'+vid_id+'-'+toa+ '.csv', index=False) # a csv file will be stored corresponding to each video_id
-
-
+            # label 1= risky object. Assigning labels corresponding to risk object.
+            df.loc[df['track_id'] == risk_objects[i], 'label'] = int(1)
+        # a csv file will be stored corresponding to each video_id
+        df.to_csv('csv_files/'+vid_id+'-'+toa + '.csv', index=False)
 
 
 # features : 100 x 35 x 512
 # detections : 100 x 34 x 6 :
-        #detections: track_id x bbox  x labels
-            # track_id : 1
-            #bbox: 4 coordinates ((normalized to 224,224 dimension) (upper_left, lower right) (w->,h))
-            #labels 1 (choice 0 : negative or 1: positive)
+        # detections: track_id x bbox  x labels
+        # track_id : 1
+        # bbox: 4 coordinates ((normalized to 224,224 dimension) (upper_left, lower right) (w->,h))
+        # labels 1 (choice 0 : negative or 1: positive)
 # vid : 1 (string)
--- feat_extract/feat_extract.py
+++ feat_extract/feat_extract.py
@@ -12,45 +12,63 @@ import torchvision.transforms as transforms
 from src.model import FeatureExtractor
 from PIL import Image
 import argparse
+import logging
 device = ("cuda" if torch.cuda.is_available() else "cpu")
 
 
 transform = transforms.Compose(
-        [   transforms.Resize(224),
-            transforms.ToTensor(),
-        ]
-    )
+    [transforms.Resize(224),
+        transforms.ToTensor(),
+     ]
+)
 to_pil_image = transforms.ToPILImage()
+
+
+def log_information(vid_id, video_frame, track_id, e):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    file_handler = logging.FileHandler('Error_log.log')
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+    file_handler.setFormatter(formatter)
+    if not logger.hasHandlers():
+        logger.addHandler(file_handler)
+    logger.info(
+        f"Error: {e}------Video_id-{vid_id}--video_frame: {video_frame} at tracking_id {track_id}")
+    return
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--csv_dir", default="csv_files") #csv files of each videos, contain tracking, bbox, etc information
-    parser.add_argument("--data_dir", default="data/") # contains the folderwise video frames
-    parser.add_argument("--feature_dir", default="feature/") # extracted resnet50 features will be stored here in *.npz format
+    # csv files of each videos, contain tracking, bbox, etc information (generated with det_csv.py)
+    parser.add_argument("--csv_dir", default="csv_files")
+    # contains the folderwise video frames
+    parser.add_argument("--data_dir", default="data/m_set1_frames/")
+    # extracted resnet50 features will be stored here in *.npz format
+    parser.add_argument("--feature_dir", default="feature/")
     args = parser.parse_args()
     return args
 
+
 def featureExt(image):
     # image = Image.open(img_path)
-    image = transform(image) # 3 x 224 x 336 (for the frame, for object it varies)
+    image = transform(image)  # 3 x 224 x 336 (for the frame, for object it varies)
     # print('shape image : ', image.shape)
-    image = torch.unsqueeze(image,0).float().to(device=device)
+    image = torch.unsqueeze(image, 0).float().to(device=device)
+    # print(image.shape)
     extractor = FeatureExtractor().to(device=device)
     extractor.eval()
     with torch.no_grad():
-        feat = extractor(image) # 1 x 2048 x 1 x 1
-        feat = torch.squeeze(feat,2) # 1 x 2048 x 1
-        feat = torch.squeeze(feat,2) # 1 x 2048
-    return feat # 1x 2048
+        feat = extractor(image)  # 1 x 2048 x 1 x 1
+        feat = torch.squeeze(feat, 2)  # 1 x 2048 x 1
+        feat = torch.squeeze(feat, 2)  # 1 x 2048
+    return feat  # 1x 2048
 
 
 def bbox_to_imroi(video_frame, bbox):
     image = Image.open(video_frame)
     image = transform(image)
-    imroi = image[:,bbox[1]:bbox[3],bbox[0]:bbox[2]] #(x1:x2, y1:y2)
+    imroi = image[:, bbox[1]:bbox[3], bbox[0]:bbox[2]]  # (x1:x2, y1:y2)
     return imroi
-
 
 
 def main():
@@ -60,18 +78,21 @@ def main():
     feature_dir = args.feature_dir
     csv_files = natsorted(glob.glob(os.path.join(csv_dir, '*.csv')))
     # print(csv_files)
-    scaling = 3.21 # 720/224
+    scaling_w = 4.82  # 1080/224
+    scaling_h = 3.21  # 720/224
 
     for i in csv_files:
-        detections =  np.zeros((100,30,6),dtype=np.float32) # 100 frames, 30 maximum objects, 6: 1-> track_id, (2,3,4,5)-> (y1,x1;y2,x2), 6-> object serial number in each frame
-        feature = np.zeros((100,31,2048),dtype=np.float32) # 100 frames, 1 global frame-level feature + 30 object level feature , resnet50 feat dimension 2048
+        # 100 frames, 30 maximum objects, 6: 1-> track_id, (2,3,4,5)-> (y1,x1;y2,x2), 6-> object serial number in each frame
+        detections = np.zeros((100, 30, 6), dtype=np.float32)
+        # 100 frames, 1 global frame-level feature + 30 object level feature , resnet50 feat dimension 2048
+        feature = np.zeros((100, 31, 2048), dtype=np.float32)
 
         vid_id_toa = i.split('.')[0].split('/')[-1]
         vid_id = vid_id_toa.split('-')[0]
-        toa = int(vid_id_toa.split('-')[-1]) # time of accident
+        toa = int(vid_id_toa.split('-')[-1])  # time of accident
         video = data_dir+vid_id+'/'
         video_frames = natsorted(glob.glob(os.path.join(video, '*.jpg')))
-        print('========',vid_id,'===================')
+        print('========', vid_id, '===================')
         for j in range(len(video_frames)):
             img_path = video_frames[j]
             image = Image.open(img_path)
@@ -79,30 +100,43 @@ def main():
             feat = featureExt(image)
             # feat = feat.detach().numpy()
             feat = feat.cpu().numpy() if feat.is_cuda else feat.detach().numpy()
-            feature[j,0,:]= feat
+            feature[j, 0, :] = feat
+        print('frame_level feature extraction finished.')
 
-        df =pd.read_csv(i)
+        df = pd.read_csv(i)
         df = df.reset_index()
-        for index,row in df.iterrows():
+        for index, row in df.iterrows():
             # print(row)
-            detections[row['frame'],row['object_num']] = row['track_id'],row['y1'],row['x1'],row['y2'],row['x2'],row['label']
+            detections[row['frame'], row['object_num']
+                       ] = row['track_id'], row['y1'], row['x1'], row['y2'], row['x2'], row['label']
             f_num = row['frame']
             video_frame = video_frames[f_num]
             if row['object_num'] > 29:
                 continue
             else:
-                bbox = [int((row['y1'])//scaling), int((row['x1'])//scaling), int((row['y2'])//scaling), int((row['x2'])//scaling)] #resizing as per the scaling factor
+                bbox = [int((row['y1'])//scaling_w), int((row['x1'])//scaling_h), int((row['y2']) //
+                                                                                      scaling_w), int((row['x2'])//scaling_h)]  # resizing as per the scaling factor
                 # print(bbox)
-                object_img = bbox_to_imroi(video_frame,bbox)
-                object_img = to_pil_image(object_img)
-                object_feat = featureExt(object_img) # 1x2048
-                # object_feat = object_feat.detach().numpy()
-                object_feat = object_feat.cpu().numpy() if object_feat.is_cuda else object_feat.detach().numpy()
+                object_img = bbox_to_imroi(video_frame, bbox)
+                # print('roi img shape', object_img.shape)
+                try:
+                    object_img = to_pil_image(object_img)
+                    newsize = (224, 224)  # resizing the rois to the same size of the input image
+                    object_img = object_img.resize(newsize)
+                    object_feat = featureExt(object_img)  # 1x2048
+                    # object_feat = object_feat.detach().numpy()
+                    object_feat = object_feat.cpu().numpy() if object_feat.is_cuda else object_feat.detach().numpy()
 
-                feature[f_num,row['object_num']+1,:] = object_feat
+                    feature[f_num, row['object_num']+1, :] = object_feat
+                except Exception as e:
+                    print('---error---')
+                    log_information(vid_id, video_frame, row['track_id'], e)
+                    continue
 
-        save_file = feature_dir+ vid_id+'.npz'
-        np.savez_compressed(save_file, feature = feature, detection = detections, vid_id= vid_id, toa=toa)
+        save_file = feature_dir + vid_id+'.npz'
+        np.savez_compressed(save_file, feature=feature,
+                            detection=detections, vid_id=vid_id, toa=toa)
+
 
 if __name__ == '__main__':
     main()
